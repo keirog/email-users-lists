6ffa9f1ee2a3fe154bee70413d3d90c66dd711ac
syymza <syymza@gmail.com>
2015-06-05 12:27:11 +0100

    Added generators for random users and lists

---
+++ fakery/lists.server.fakery.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/**
+ * Generates fake data for the List model
+ */
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+// External modules
+require('dotenv').load({silent: true});
+const mongoose = require('mongoose');
+const fakery = require('mongoose-fakery');
+const faker = require('faker');
+const async = require('async');
+
+const db = require('../config/mongoose')();
+const List = mongoose.model('List');
+
+function createFakeList (next) {
+
+    fakery.fake('list', List, {
+        name: faker.lorem.sentence(),
+        description: faker.lorem.paragraph()
+    });
+
+    fakery.makeAndSave('list', (err, list) =>  {
+        console.log(list);
+        next();
+    });
+}
+
+let listsToCreate = 200;
+let count = 0;
+
+List.remove(() => {
+    async.whilst( () => { return (count < listsToCreate); }, (next) => {
+        count++;
+        createFakeList(next);
+    }, (err) => {
+        if (err) { console.log(err); }
+        else { console.log(listsToCreate, 'lists added'); }
+        process.exit();
+    });
+});
+
+
