f450cd5e78b9bcc1630d8471cf305f70d92cee16
syymza <syymza@gmail.com>
2015-06-09 17:24:28 +0100

    Created crypto util

---
+++ tests/crypto.server.utils.test.js
@@ -0,0 +1,20 @@
+'use strict';
+
+require('dotenv').load({silent: true});
+const should = require('should');
+
+const encrypt = require('../app/utils/crypto.server.utils').encrypt;
+const decrypt = require('../app/utils/crypto.server.utils').decrypt;
+
+describe('The crypto util', () => {
+
+    it('is reversible', (done) => {
+
+        let message = 'exampleemail@example.com';
+        console.log(decrypt(encrypt(message)));
+        decrypt(encrypt(message)).should.match(message);
+        done();
+
+    });
+
+});
