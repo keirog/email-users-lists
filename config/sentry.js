5e19ea8fce7e5bf3b840ae7f3d8c01592948aed3
syymza <syymza@gmail.com>
2015-06-11 09:53:50 +0100

    Return sentry object on init()

--- config/sentry.js
+++ config/sentry.js
@@ -4,6 +4,6 @@ exports.init = () => {
     const dsn = process.env.SENTRY_DSN;
     if (dsn) {
         const raven = require('raven');
-        const client = new raven.Client(dsn);
+        return new raven.Client(dsn);
     }
 };
