dcd984f45d454019cf951fa9cba61ecdb16793d4
syymza <syymza@gmail.com>
2015-06-18 14:45:34 +0100

    Added test text to health check

--- app/controllers/__health.server.controller.js
+++ app/controllers/__health.server.controller.js
@@ -7,7 +7,14 @@ exports.handle = (req, res) => {
     health.schemaVersion = 1;
     health.name = 'Email Platform Users-Lists';
     health.description = 'Email Platform Users-Lists REST API';
-    health.checks = [];
+    health.checks = [{ // Random check
+        name: "The Application is UP",
+        ok: true,
+        severity: 2,
+        businessImpact: 'Some test text',
+        technicalSummary: 'Some test text',
+        panicGuide: 'Some test text'
+    }];
 
     res.status(200).json(health);
 };
