b48c971329a1a592e58b22862aec11f74bffff45
syymza <syymza@gmail.com>
2015-07-10 11:13:12 +0100

    Added production config

---
+++ config/env/production.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const port = process.env.PORT;
+const logLevel = process.env.LOG_LEVEL || 'error';
+const processId = process.env.DYNO;
+const db = process.env.MONGOLAB_URI;
+const authUser = process.env.BASIC_AUTH_USER;
+const authPassword = process.env.BASIC_AUTH_PASSWORD;
+
+module.exports = {
+    port: port,
+    db: db,
+    processId: processId,
+    logLevel: logLevel,
+    authUser: authUser,
+    authPassword: authPassword
+};
