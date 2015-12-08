573127c7ab664d0059949908511ff27fc45dddd1
syymza <syymza@gmail.com>
2015-12-08 12:07:57 +0000

    Migrated to mongo hq

--- config/env/development.js
+++ config/env/development.js
@@ -3,7 +3,7 @@
 const port = process.env.PORT || 1337;
 const logLevel = process.env.LOG_LEVEL || 'info';
 const processId = process.env.DYNO || process.pid;
-const db = process.env.MONGOLAB_URI || 'mongodb://localhost/ft-email-users-lists-dev';
+const db = process.env.MONGOHQ_URL || 'mongodb://localhost/ft-email-users-lists-dev';
 const authUser = process.env.BASIC_AUTH_USER || 'development';
 const authPassword = process.env.BASIC_AUTH_PASSWORD || 'development';
 const unsubscribeSecret = process.env.UNSUBSCRIBE_SECRET || 'developmentSecret';
--- config/env/production.js
+++ config/env/production.js
@@ -3,7 +3,7 @@
 const port = process.env.PORT;
 const logLevel = process.env.LOG_LEVEL || 'error';
 const processId = process.env.DYNO;
-const db = process.env.MONGOLAB_URI;
+const db = process.env.MONGOHQ_URL;
 const authUser = process.env.BASIC_AUTH_USER;
 const authPassword = process.env.BASIC_AUTH_PASSWORD;
 const unsubscribeSecret = process.env.UNSUBSCRIBE_SECRET;
