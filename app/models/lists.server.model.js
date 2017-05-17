0fce103de161da107edbe2b7d1ff42d3ed07f8b3
mara <mara.wanot@ft.com>
2017-05-17 16:36:30 +0100

    added image url and frequency to model

--- app/models/lists.server.model.js
+++ app/models/lists.server.model.js
@@ -1,44 +1,51 @@
-'use strict';
-
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
 const ListSchema = new Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: 'name cannot be blank'
+  name: {
+    type: String,
+    trim: true,
+    required: 'name cannot be blank',
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  inactive: {
+    type: Boolean,
+    default: false,
+    index: true,
+  },
+  subscriptionLevel: {
+    type: String,
+    enum: ['Registered', 'Standard', 'Premium'],
+    default: 'Registered',
+    trim: true,
+
+  },
+  externalIds: {
+    eBay: {
+      type: String,
+      trim: true,
     },
-    description: {
-        type: String,
-        trim: true
-    },
-    inactive: {
-        type: Boolean,
-        default: false,
-        index: true
-    },
-    subscriptionLevel: {
-        type: String,
-        enum: ['Registered', 'Standard', 'Premium'],
-        default: 'Registered',
-        trim: true
-        
-    },
-    externalIds: {
-        eBay: {
-            type: String,
-            trim: true
-        }
-    },
-    consolidatedList: {
-        type: Schema.Types.ObjectId,
-        ref: 'List',
-        index: true
-    }
+  },
+  consolidatedList: {
+    type: Schema.Types.ObjectId,
+    ref: 'List',
+    index: true,
+  },
+  imageURL: {
+    type: String,
+    trim: true,
+  },
+  frequency: {
+    type: String,
+    trim: true,
+    maxlength: 50,
+  },
 }, {
-    read: 'secondaryPreferred'
+  read: 'secondaryPreferred',
 });
 
 mongoose.model('List', ListSchema);
